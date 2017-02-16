76da8e149013833c9311d747360d3da4480cc8f1
hawaihara <hawaihara@IBM896-PE01RCYY>
2017-02-16 17:26:08 -0200

    teste22

--- src/dotnetstarter/Startup.cs
+++ src/dotnetstarter/Startup.cs
@@ -1,13 +1,31 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 public class Startup
 {
-	public void Configure(IApplicationBuilder app)
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Add framework services.
+        //services.AddApplicationInsightsTelemetry(Configuration);
+
+        services.AddMvc();
+    }
+
+    public void Configure(IApplicationBuilder app)
 	{
 		app.UseDefaultFiles();
 		app.UseStaticFiles();
+
+        app.UseMvc(routes =>
+        {
+            routes.MapRoute(
+                   name: "default",
+                   template: "{controller=Home}/{action=Index}/{id?}");
+        });
     }
 
     public static void Main(string[] args)
